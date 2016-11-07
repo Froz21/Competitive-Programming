c3bfe1c8e211315de179c8066fe9d78f88c84994
Froz21 <ariangallardo21@gmail.com>
2016-11-07 08:32:04 -0500

    Added tarjan.cpp

---
+++ Team_Templates/tarjan.cpp
@@ -0,0 +1,25 @@
+//Algoritmo de Tarjan: Componentes fuertemente conexas
+
+stack<int> stk;
+int id[N];
+int low[N];
+
+void dfs(int u){
+	stk.push(u);
+	id[u]=curr_id++;
+	low[u]=u;
+	REP(i,0,adj[u].size()){
+		int v = adj[u][i];
+		if(id[v]==-1){
+			dfs(v);
+			low[u]=min(low[u],low[v]);
+		}
+		else low[u]=min(low[u],id[v]);
+	}
+	vi cmp;
+	int a;
+	do{
+		v=stk.top(); stk.pop();
+		cmp.pb(v);
+	}while(v!=u);
+}
